fef228d16b7cb0fd13b0bf3cdd61f9de160516dc
ayushrawat <ayush.rawat@kaarya.com>
2018-03-24 22:57:19 +0530

    TEST.PUBLISHER.8

--- src/main/java/com/mykaarma/test/publisher/runner/DocumentServiceController.java
+++ src/main/java/com/mykaarma/test/publisher/runner/DocumentServiceController.java
@@ -1,7 +1,5 @@
 package com.mykaarma.test.publisher.runner;
 
-import java.io.IOException;
-import java.nio.file.Files;
 import java.util.Date;
 import java.util.UUID;
 
@@ -10,8 +8,6 @@ import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,26 +21,18 @@ public class DocumentServiceController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(DocumentServiceController.class);
 	@Autowired
 	private Sender documentService;
-	@Autowired
-	private ResourceLoader resourceLoader;
 	
-	String xml = null, toConcat = "</CheckSum></RepairOrderWrap>";
+	String xml = "<RepairOrderWrap><Dealer><DealerID/><DealerName/><DealerDMS><DealerDMSID/><DMSName/><UserName/><Password/><HostUrl/></DealerDMS></Dealer><Source>AdpPdfManager</Source><Invoice/><LaborTypes><LaborType>CP</LaborType></LaborTypes><RepairOrder><ID/><OrderNumber>286952</OrderNumber><OrderType>RO</OrderType><Amount/><OrderDate>2017-08-10 14:26:02</OrderDate><AssociateDMSID>995952</AssociateDMSID><DealerAssociateID/><Description/><OrderStatus>READY TO POST</OrderStatus><NumberOfInvoices/><ReadyROData/><IsPaid/><PaidAmount/><IsPaidInKaarma/><IsPaymentRequestSent/><Tag>TO0966</Tag><MileageText/><InvoiceUrl/><PdfRequestAmtRef/><LabourOpTypes>INFLATE,MISC,MPIS</LabourOpTypes><DepartmentType>S</DepartmentType><CustPayTotal>260.58</CustPayTotal></RepairOrder><CustWrap><CAttrs><IsBusiness/><AssignedSA/><Comments/></CAttrs><IdentityAttrs><Attr value=\"SSN\"/><Attr value=\"DriverLicense\"/><Attr value=\"BirthDate\"/><Attr value=\"Gender\"/><Attr value=\"Language\"/></IdentityAttrs><CustPref><Pref value=\"\"/><Pref value=\"text\">Y</Pref><Pref value=\"email\">Y</Pref><Pref value=\"call\">Y</Pref><Pref value=\"phone\"/><Pref value=\"postal\"/></CustPref><PreferredComm><Preferred><Mode/><Value/><Time/></Preferred><Preferred><Mode/><Value/><Time/></Preferred></PreferredComm><Customer><ID/><Version/><CustomerKey/><TypeCode/><CustomerType/><DealerID/><FName>Samir</FName><MName/><LName>Khoury</LName><ImageUrl/><Indexed/><Valid/><OptedOut/><UpdatedBy/><CreatedBy/><Communications><Communication><ID/><Version/><Type>E</Type><Value>KHOURY418@HOTMAIL.COM</Value><CreatedBy/><Preferred/><Valid>1</Valid><Label>work</Label><Desc/><UpdatedBy/><CommPrefs/></Communication><Communication><ID/><Version/><Type>P</Type><Value>(626)863-5805</Value><CreatedBy/><Preferred/><Valid>1</Valid><Label>Cell</Label><Desc/><UpdatedBy/><CommPrefs/></Communication><Communication><ID/><Version/><Type>P</Type><Value>(626)804-3530</Value><CreatedBy/><Preferred/><Valid>1</Valid><Label>Home</Label><Desc/><UpdatedBy/><CommPrefs/></Communication><Communication><ID/><Version/><Type>P</Type><Value>(626)863-5805</Value><CreatedBy/><Preferred/><Valid>1</Valid><Label>Work</Label><Desc/><UpdatedBy/><CommPrefs/></Communication></Communications><Addresses/></Customer><LastModified/><StatusCode/><StatusMessage/></CustWrap>", toConcat = "</CheckSum></RepairOrderWrap>";
 	
 	@PostConstruct
-	void loadXML() throws IOException{
-		Resource resource = resourceLoader.getResource("classpath:286952.xml");
-		try {
-			xml = new String(Files.readAllBytes(resource.getFile().toPath()));
-			LOGGER.info("XML Loaded into Memory");
-		} catch (IOException e) {
-			throw e;
-		}
+	void Log(){
+		LOGGER.info("App Loaded!");
 	}
-	
 	
 	@RequestMapping(value="/saveKaarmaDealerOrderXml", method=RequestMethod.GET)
 	public String saveKaarmaDealerOrderXML()  throws Exception
 	{
+		String ros = "";
 		for(int i=0; i<100; i++) {
 			String msgId = UUID.randomUUID().toString();
 			String finXml = xml + "<CheckSum>"+msgId+toConcat;
@@ -56,7 +44,8 @@ public class DocumentServiceController {
 			kaarmaEntityXML.setOriginDate(new Date());
 			LOGGER.info("Pushing "+msgId+" into Mongo");
 			documentService.saveKaarmaROXMLAndPublishToDocQueue(kaarmaEntityXML);
+			ros+="TESTING-"+i;
 		}
-		return null;
+		return ros;
 	}
 }
